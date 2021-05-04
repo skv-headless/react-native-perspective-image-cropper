4c4c96e5892b5607f0e21277f0e9f551fdb6b4a0
Evgeny Sokovikov <skv-headless@yandex.ru>
2021-05-04 10:48:33 +0400

    run in bg via gcd

--- ios/RCTCustomCropManager.mm
+++ ios/RCTCustomCropManager.mm
@@ -9,35 +9,33 @@ RCT_EXPORT_METHOD(crop:(NSDictionary *)points imageUri:(NSString *)imageUri call
 {
     NSString *parsedImageUri = [imageUri stringByReplacingOccurrencesOfString:@"file://" withString:@""];
     NSURL *fileURL = [NSURL fileURLWithPath:parsedImageUri];
-    CIImage *ciImage = [CIImage imageWithContentsOfURL:fileURL];
-    
+
     CGPoint newLeft = CGPointMake([points[@"topLeft"][@"x"] floatValue], [points[@"topLeft"][@"y"] floatValue]);
     CGPoint newRight = CGPointMake([points[@"topRight"][@"x"] floatValue], [points[@"topRight"][@"y"] floatValue]);
     CGPoint newBottomLeft = CGPointMake([points[@"bottomLeft"][@"x"] floatValue], [points[@"bottomLeft"][@"y"] floatValue]);
     CGPoint newBottomRight = CGPointMake([points[@"bottomRight"][@"x"] floatValue], [points[@"bottomRight"][@"y"] floatValue]);
-    
+
     newLeft = [self cartesianForPoint:newLeft height:[points[@"height"] floatValue] ];
     newRight = [self cartesianForPoint:newRight height:[points[@"height"] floatValue] ];
     newBottomLeft = [self cartesianForPoint:newBottomLeft height:[points[@"height"] floatValue] ];
     newBottomRight = [self cartesianForPoint:newBottomRight height:[points[@"height"] floatValue] ];
-    
-    
+
     
     NSMutableDictionary *rectangleCoordinates = [[NSMutableDictionary alloc] init];
-    
+
     rectangleCoordinates[@"inputTopLeft"] = [CIVector vectorWithCGPoint:newLeft];
     rectangleCoordinates[@"inputTopRight"] = [CIVector vectorWithCGPoint:newRight];
     rectangleCoordinates[@"inputBottomLeft"] = [CIVector vectorWithCGPoint:newBottomLeft];
     rectangleCoordinates[@"inputBottomRight"] = [CIVector vectorWithCGPoint:newBottomRight];
-    
-    ciImage = [ciImage imageByApplyingFilter:@"CIPerspectiveCorrection" withInputParameters:rectangleCoordinates];
-    
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CGImageRef cgimage = [context createCGImage:ciImage fromRect:[ciImage extent]];
-    UIImage *image = [UIImage imageWithCGImage:cgimage];
-    
-    NSData *imageToEncode = UIImageJPEGRepresentation(image, 0.8);
-    callback(@[[NSNull null], @{@"image": [imageToEncode base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength]}]);
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        CIImage *ciImage = [CIImage imageWithContentsOfURL:fileURL];
+        ciImage = [ciImage imageByApplyingFilter:@"CIPerspectiveCorrection" withInputParameters:rectangleCoordinates];
+        UIImage *image = [[UIImage alloc] initWithCIImage:ciImage];
+
+        NSData *imageToEncode = UIImageJPEGRepresentation(image, 0.8);
+        callback(@[[NSNull null], @{@"image": [imageToEncode base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength]}]);
+    });
 }
 
 - (CGPoint)cartesianForPoint:(CGPoint)point height:(float)height {
@@ -45,3 +43,4 @@ RCT_EXPORT_METHOD(crop:(NSDictionary *)points imageUri:(NSString *)imageUri call
 }
 
 @end
+
